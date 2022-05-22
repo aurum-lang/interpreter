105537292b2747222fc0a44a3986c1994cead00c
Raforawesome <47924183+Raforawesome@users.noreply.github.com>
2022-05-22 14:11:50 -0400

    Avoided panic on lexer test

--- src/lib.rs
+++ src/lib.rs
@@ -7,7 +7,10 @@ mod tests {
     #[test]
     fn lexer_test() {
         let read: String = std::fs::read_to_string("test.au").unwrap();
-		let result: Vec<u8> = lexer::tokenize(read).unwrap();
+		let result: Vec<u8> = lexer::tokenize(read).unwrap_or_else(|e| {
+			println!("{}", e.msg);
+			std::process::exit(1);
+		});
 
 		let _ = std::fs::create_dir("compile/");
 		let _ = std::fs::write("compile/c.lex", result);
